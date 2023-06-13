e6a8a450d69ab327616be344a7981030e5dd14d6
valeriofilareti <valerio.filareti@gmail.com>
2023-06-13 18:55:28 +0200

    [add] input value

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -21,42 +21,42 @@
 
         <div class="form-group my-3">
             <label for="title">Title</label>
-            <input name="title" type="text" class="form-control @error('title') is-invalid @enderror" id="title" placeholder="Enter Title">
+            <input value="{{old('title')}}" name="title" type="text" class="form-control @error('title') is-invalid @enderror" id="title" placeholder="Enter Title">
         </div>
         @error('title')
         <p>{{$message}}</p>
         @enderror
         <div class="form-group my-3">
             <label for="thumb">Thumb</label>
-            <input name="thumb" type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" placeholder="Enter Thumb">
+            <input value="{{old('thumb')}}" name="thumb" type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" placeholder="Enter Thumb">
         </div>
         @error('thumb')
         <p>{{$message}}</p>
         @enderror
         <div class="form-group my-3">
             <label for="price">Price</label>
-            <input name="price" type="text" class="form-control @error('price') is-invalid @enderror" id="price" placeholder="Enter Price">
+            <input value="{{old('price')}}" name="price" type="text" class="form-control @error('price') is-invalid @enderror" id="price" placeholder="Enter Price">
         </div>
         @error('price')
         <p>{{$message}}</p>
         @enderror
         <div class="form-group my-3">
             <label for="series">Series</label>
-            <input name="series" type="text" class="form-control @error('series') is-invalid @enderror" id="series" placeholder="Enter Series">
+            <input value="{{old('series')}}" name="series" type="text" class="form-control @error('series') is-invalid @enderror" id="series" placeholder="Enter Series">
         </div>
         @error('series')
         <p>{{$message}}</p>
         @enderror
         <div class="form-group my-3">
             <label for="type">Type</label>
-            <input name="type" type="text" class="form-control @error('type') is-invalid @enderror" id="type" placeholder="Enter Type">
+            <input value="{{old('type')}}" name="type" type="text" class="form-control @error('type') is-invalid @enderror" id="type" placeholder="Enter Type">
         </div>
         @error('type')
         <p>{{$message}}</p>
         @enderror
         <div class="form-group my-3">
             <label for="description">Description</label>
-            <input name="description" type="text-area" class="form-control @error('description') is-invalid @enderror" id="description" placeholder="Enter Description">
+            <input value="{{old('description')}}" name="description" type="text-area" class="form-control @error('description') is-invalid @enderror" id="description" placeholder="Enter Description">
         </div>
         @error('description')
         <p>{{$message}}</p>
